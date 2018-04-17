520987ade190369123bd84ae6d3d8eea96f4d5aa
Broken-Dreams <33333799+Broken-Dreams@users.noreply.github.com>
2018-04-17 16:38:34 +1200

    Code to log into Instagram written and code cleaned up.

--- src/IGMain.java
+++ src/IGMain.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class IGMain {
 
 	public static void main(String[] args) {
 
+		//driverIG.getDriver();
 		
+		
+		
+		/*Scanner nameInput = new Scanner(System.in);
+		System.out.println("Enter Instagram name: ");
+		String name = nameInput.next();
+		nameInput.close();
+		
+		Scanner passInput = new Scanner(System.in);  
+		System.out.println("Enter Instagram password: ");
+		String pass = passInput.next();
+		passInput.close();*/
+		
+		//set up page to connect to
+		loginIG.login("apples", "bananas");
 		//connect to IG
 		
 		
--- src/loginIG.java
+++ src/loginIG.java
@@ -1,16 +1,31 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class loginIG {
 
-	//get username
-	
-	
-	//get password
-	
-	
+	public static void login(String userName, String passWord) {
+
+		//gets driver to make Firefox work and loads IG login page.
+		WebDriver driverReturned = driverIG.getDriver();
+		driverReturned.get("https://www.instagram.com/accounts/login/");
+
+		//allows the page to finish building all frames (otherwise you will get a 
+		//NoElementException error). 
+		driverReturned.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+		//finds the id of the text area and sends username/pass to each text area
+		WebElement userArea = driverReturned.findElement(By.xpath("//input[@name='username']"));
+		userArea.sendKeys(userName);
+		WebElement userPass = driverReturned.findElement(By.xpath("//input[@name='password']"));
+		userPass.sendKeys(passWord);
+
+	}
 	//ask if username and password will be saved for future
-	
-	
-	//output username and password to instagram.
-	
-	
 }
